441a502d7e44bb7ad650bdba675848dbb53d02f0
tanch1eu <dchieu.nt@gmail.com>
2022-10-25 15:28:13 +1000

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/app/app.component.html
    	modified:   src/app/app.component.ts
    	modified:   src/app/app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import {
+  CalendarOptions,
+  defineFullCalendarElement,
+} from '@fullcalendar/web-component';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import listPlugin from '@fullcalendar/list';
+import timeGridPlugin from '@fullcalendar/timegrid';
+// make the <full-calendar> element globally available
+defineFullCalendarElement();
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'fullCld';
+  calendarVisible = true;
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin, listPlugin, timeGridPlugin],
+    initialView: 'dayGridMonth',
+    headerToolbar: {
+      left: 'prevYear,prev,today,next,nextYear',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+    },
+  };
+
+  toggleWeekends() {
+    this.calendarOptions = {
+      ...this.calendarOptions,
+      weekends: !this.calendarOptions.weekends,
+    };
+  }
 }
